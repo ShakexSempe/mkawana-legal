740077165f3a008cf5b4f2d1c4afcac6bdcea683
Ray-Shakex <bshakex@gmail.com>
2022-05-12 11:45:12 +0200

    js and sass

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-alert();
+// TOGGLE VARIABLES
+const toggle = document.querySelector('.toggle');
+const mobileMenu = document.querySelector('.mobile-links');
+console.log(mobileMenu);
+
+toggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active-menu");
+});
+
+
+const header = document.querySelector('.header');
+const hero = document.querySelector('.hero');
+const heroOptions = {
+    rootMargin: "-90% 0px 0px 0px",
+}
+const heroObserver = new IntersectionObserver(
+    function(entries, heroObserver){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                console.log("hero NOT Intersecting");
+                header.classList.remove("active-hero")
+            } else {
+                console.log('hero is intersecting');
+                header.classList.add("active-hero")
+            }
+        })
+    }, heroOptions
+);
+heroObserver.observe(hero);
